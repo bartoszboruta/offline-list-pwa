279c2782165439b24970c82c7260fdf8c73a80f4
bartoszboruta <bboruta.dev@gmail.com>
2018-12-19 15:53:51 +0100

    Updates

---
+++ precache-manifest.96137b93c44ba2017ab61d12469bc3e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/offline-list-pwa/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1eaa4b132706d877dea4",
+    "url": "/offline-list-pwa/static/css/main.b3696585.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/offline-list-pwa/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "247ac1d464ae02030b12",
+    "url": "/offline-list-pwa/static/js/1.247ac1d4.chunk.js"
+  },
+  {
+    "revision": "70e76f5779b6b6e302d3",
+    "url": "/offline-list-pwa/static/js/runtime~main.70e76f57.js"
+  },
+  {
+    "revision": "1eaa4b132706d877dea4",
+    "url": "/offline-list-pwa/static/js/main.1eaa4b13.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/offline-list-pwa/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/offline-list-pwa/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/offline-list-pwa/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "247ac1d464ae02030b12",
+    "url": "/offline-list-pwa/static/css/1.7e19653c.chunk.css"
+  },
+  {
+    "revision": "21f5a0c18c2b1bd719d45fcf5f2d4ede",
+    "url": "/offline-list-pwa/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/offline-list-pwa/precache-manifest.96137b93c44ba2017ab61d12469bc3e6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/offline-list-pwa/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
